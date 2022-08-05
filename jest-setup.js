1fd944646bae7592f16f8344610d359f20d439f6
codefreak13 <ezeokecc@gmail.com>
2022-08-05 11:46:10 +0100

    setup test suites

---
+++ jest-setup.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock';
+
+// const mockAsyncStorage = require('@react-native-community/async-storage/jest/async-storage-mock');
+jest.mock('@react-native-community/async-storage', () => mockAsyncStorage);
+
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {};
+
+  return Reanimated;
+});
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+jest.mock('react-native/Libraries/Utilities/Platform', () => {
+  const platform = jest.requireActual(
+    'react-native/Libraries/Utilities/Platform',
+  );
+  return {
+    ...platform,
+    constants: {
+      ...platform.constants,
+      reactNativeVersion: {
+        major: 0,
+        minor: 65,
+        patch: 1,
+      },
+    },
+  };
+});
